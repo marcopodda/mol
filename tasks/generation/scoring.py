8e1bfe78d5b6432e5be68c71130c007553202872
Marco Podda <marco.podda@di.unipi.it>
2020-08-06 17:10:11 +0200

    updated

---
+++ tasks/generation/scoring.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+from rdkit import Chem
+from moses import get_all_metrics
+
+from core.mols.split import join_fragments
+from core.utils.vocab import Vocab
+from core.utils.serialization import load_yaml
+
+
+def score(tokens_path, n_jobs=40):
+    vocab = Vocab.from_file(output_dir / "DATA" / "vocab.csv")
+    tokens_list = load_yaml(tokens_path)
+    
+    mols = []
+    for tokens in tokens_list:
+        frags = [Chem.MolFromSmiles(vocab[t]) for t in tokens]
+        try:
+            mols.append(join_fragments(frags))
+        except:
+            pass
+    
+    smis = [Chem.MolToSmiles(m) for m in mols]
+    return get_all_metrics(smis, n_jobs=n_jobs)
+        
+    
+    
+    
