386d6ac0c1bcd508a2c0ef9cd2b979f2caf1ed4d
Marco Podda <marco.podda@di.unipi.it>
2020-08-09 15:27:30 +0200

    updated

--- layers/graphconv.py
+++ layers/graphconv.py
@@ -12,25 +12,33 @@ from layers.mlp import MLP
 class GNN(nn.Module):
     def __init__(self, hparams):
         super().__init__()
-        self.num_layers = hparams.gnn_num_layers
+        self.hparams = hparams
+        
         self.dim_input = ATOM_FDIM
         self.dim_hidden = hparams.gnn_dim_hidden
+        self.dim_hidden_edge = hparams.gnn_dim_hidden_edge
         self.dim_embed = hparams.gnn_dim_embed
+        
+        self.num_layers = hparams.gnn_num_layers
 
         self.convs = nn.ModuleList([])
+        self.bns = nn.ModuleList([])
         
-        edge_net = MLP(
+        self.edge_net = MLP(
             dim_input=BOND_FDIM,
-            dim_hidden=self.hparams.gnn_dim_hidden_edge,
+            dim_hidden=self.dim_hidden_edge,
             dim_output=BOND_FDIM * self.dim_hidden
         )
         
         for i in range(self.num_layers):
             dim_input = self.dim_input if i == 0 else self.dim_hidden
-            conv = NNConv(in_channels=dim_input, out_channels=self.dim_hidden, nn=edge_net)
+            
+            conv = NNConv(in_channels=dim_input, out_channels=self.dim_hidden, nn=self.edge_net)
             self.convs.append(conv)
+            
+            bn = nn.BatchNorm1d(self.dim_hidden, track_running_stats=False)
+            self.bns.append(bn)
 
-        self.bn = BatchNorm(self.dim_hidden, track_running_stats=False)
         self.readout = nn.Linear(self.dim_hidden, self.dim_embed)
 
         for p in self.parameters():
@@ -41,9 +49,9 @@ class GNN(nn.Module):
         x, edge_index, edge_attr, batch = data.x, data.edge_index, data.edge_attr, data.batch
 
         outputs = []
-        for i, conv in enumerate(self.convs):
+        for conv, bn in zip(self.convs, self.bns):
             x = conv(x, edge_index, edge_attr=edge_attr)
-            x = self.bn(F.relu(x))
+            x = F.relu(bn(x))
         
         output = global_add_pool(x, batch)
         return self.readout(output)
