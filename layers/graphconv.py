e1445fb2559801d1ebb6759fdb693e7e8293b2db
Marco Podda <marco.podda@di.unipi.it>
2020-08-09 15:15:54 +0200

    updated

--- layers/graphconv.py
+++ layers/graphconv.py
@@ -9,23 +9,6 @@ from core.datasets.features import ATOM_FDIM, BOND_FDIM
 from layers.mlp import MLP
 
 
-class GNNLayer(nn.Module):
-    def __init__(self, hparams, dim_input, edge_net):
-        super().__init__()
-        self.dim_input = dim_input
-        self.dim_hidden = hparams.gnn_dim_hidden_edge
-        self.dim_output = hparams.gnn_dim_hidden
-
-        self.conv = NNConv(
-            in_channels=self.dim_input,
-            out_channels=self.dim_output,
-            nn=edge_net
-        )
-
-    def forward(self, x, edge_index, edge_attr):
-        return self.conv(x, edge_index, edge_attr)
-
-
 class GNN(nn.Module):
     def __init__(self, hparams):
         super().__init__()
@@ -34,16 +17,18 @@ class GNN(nn.Module):
         self.dim_hidden = hparams.gnn_dim_hidden
         self.dim_embed = hparams.gnn_dim_embed
 
-        self.layers = nn.ModuleList([])
-        self.edge_net = MLP(
+        self.convs = nn.ModuleList([])
+        
+        edge_net = MLP(
             dim_input=BOND_FDIM,
-            dim_hidden=self.dim_hidden,
-            dim_output=self.dim_input * self.dim_output
+            dim_hidden=self.hparams.gnn_dim_hidden_edge,
+            dim_output=BOND_FDIM * self.dim_hidden
         )
         
         for i in range(self.num_layers):
             dim_input = self.dim_input if i == 0 else self.dim_hidden
-            self.layers.append(GNNLayer(hparams, dim_input, self.edge_net))
+            conv = NNConv(in_channels=dim_input, out_channels=self.dim_hidden, nn=edge_net)
+            self.convs.append(conv)
 
         self.bn = BatchNorm(self.dim_hidden, track_running_stats=False)
         self.readout = nn.Linear(self.dim_hidden, self.dim_embed)
@@ -56,8 +41,8 @@ class GNN(nn.Module):
         x, edge_index, edge_attr, batch = data.x, data.edge_index, data.edge_attr, data.batch
 
         outputs = []
-        for i, layer in enumerate(self.layers):
-            x = layer(x, edge_index, edge_attr=edge_attr)
+        for i, conv in enumerate(self.convs):
+            x = conv(x, edge_index, edge_attr=edge_attr)
             x = self.bn(F.relu(x))
         
         output = global_add_pool(x, batch)
