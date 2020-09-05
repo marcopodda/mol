ac1fe316bbc7531c71e5b5770c89ccc2351f7f6c
Marco Podda <marco.podda@di.unipi.it>
2020-09-05 20:04:40 +0200

    ok

--- tasks/translation/experiment.py
+++ tasks/translation/experiment.py
@@ -99,10 +99,10 @@ def run(args):
         fast_dev_run=args.debug,
         logger=logger,
         gpus=gpu)
-    path = output_dir.parent / "moses" / "generation" / "checkpoints" / "epoch=30.ckpt"
-    ckpt = GenerationPLWrapper.load_from_checkpoint(path.as_posix(), output_dir=output_dir.parent / "moses", name=args.dataset_name)
-    train_model = PLWrapper(hparams, output_dir, args.dataset_name)
-    train_model.model.embedder.gnn = ckpt.model.embedder.gnn
+    # path = output_dir.parent / "moses" / "generation" / "checkpoints" / "epoch=30.ckpt"
+    # ckpt = GenerationPLWrapper.load_from_checkpoint(path.as_posix(), output_dir=output_dir.parent / "moses", name=args.dataset_name)
+    # train_model = PLWrapper(hparams, output_dir, args.dataset_name)
+    # train_model.model.embedder.gnn = ckpt.model.embedder.gnn
     trainer.fit(train_model)
         
 
