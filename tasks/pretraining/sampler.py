e32ecf4aaa0e337617cacc658d8ab16ef9b1f06d
Marco Podda <marco.podda@di.unipi.it>
2020-09-06 16:46:38 +0200

    ok

--- tasks/pretraining/sampler.py
+++ tasks/pretraining/sampler.py
@@ -6,7 +6,7 @@ from tasks.pretraining.loader import PretrainingDataLoader
 class PretrainingSampler(Sampler):
     def get_loader(self, batch_size=128):
         loader = PretrainingDataLoader(self.hparams, self.dataset)
-        num_samples = min(len(self.dataset.val_indices), 1000)
+        num_samples = min(len(self.dataset.val_indices), 30000)
         indices = sorted(np.random.choice(self.dataset.val_indices, num_samples, replace=False))
         smiles = self.dataset.data.loc[indices].smiles.tolist()
         return smiles, loader.get_loader(indices, batch_size=batch_size)
