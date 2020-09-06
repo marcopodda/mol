05f5406c9ccbe3c96a1a0821aff42a9e2386be8c
Marco Podda <marco.podda@di.unipi.it>
2020-09-06 20:50:45 +0200

    ok

--- layers/sampler.py
+++ layers/sampler.py
@@ -135,7 +135,7 @@ class Sampler:
             
             x = embedder(indexes)
             x = x.view(batch_size, 1, -1)
-            
+        print(samples)
         frags = self.translate(samples)
         return frags
     
