699cf0bbcbb239a5b5ff3d6cb7c776138e368145
Marco Podda <marco.podda@di.unipi.it>
2020-09-09 16:33:14 +0200

    ok

--- layers/autoencoder.py
+++ layers/autoencoder.py
@@ -11,34 +11,42 @@ FINGERPRINT_DIM = 2048
 class Autoencoder(nn.Module):
     def __init__(self, hparams, output_dir):
         super().__init__()
-        
+
         if isinstance(hparams, dict):
             hparams = Namespace(**hparams)
-            
+
         self.hparams = hparams
         self.output_dir = output_dir
         self.dim_hidden = hparams.rnn_dim_state
-        
+
         assert self.dim_hidden < FINGERPRINT_DIM // 4
-    
+
         self.input = nn.Linear(FINGERPRINT_DIM, FINGERPRINT_DIM // 2)
         self.input2hidden = nn.Linear(FINGERPRINT_DIM // 2, FINGERPRINT_DIM // 4)
         self.hidden2bottleneck = nn.Linear(FINGERPRINT_DIM // 4, self.dim_hidden)
         self.bottleneck2hidden = nn.Linear(self.dim_hidden, FINGERPRINT_DIM // 4)
         self.hidden2output = nn.Linear(FINGERPRINT_DIM // 4, FINGERPRINT_DIM // 2)
         self.output = nn.Linear(FINGERPRINT_DIM // 2, FINGERPRINT_DIM)
-    
+
     def encode(self, inputs):
         x = self.input(inputs)
         x = self.input2hidden(F.relu(x))
         return self.hidden2bottleneck(F.relu(x))
-    
+
     def decode(self, hidden):
         x = self.bottleneck2hidden(hidden)
         x = self.hidden2output(F.relu(x))
         return self.output(F.relu(x))
-    
-    def forward(self, batch):
+
+    def forward(self, batch, denoise=True):
+        if denoise is True:
+            batch = self.add_noise(batch)
         hidden = self.encode(batch)
         output = self.decode(hidden)
         return torch.sigmoid(output), hidden
+
+    def add_noise(self, batch):
+        noisy_batch = batch.clone()
+        noise_mask = torch.rand_like(noisy_batch) <= self.hparams.autoencoder_noise
+        noisy_batch[noise_mask] = torch.logical_not(noisy_batch[noise_mask]).float()
+        return noisy_batch
--- tasks/translation/model.py
+++ tasks/translation/model.py
@@ -11,13 +11,13 @@ from layers.decoder import Decoder
 from layers.autoencoder import Autoencoder
 
 
-class TranslationModel(nn.Module):  
+class TranslationModel(nn.Module):
     def __init__(self, hparams, output_dir, vocab_size, max_length):
         super().__init__()
-        
+
         if isinstance(hparams, dict):
             hparams = Namespace(**hparams)
-        
+
         self.hparams = hparams
         self.output_dir = output_dir
         self.num_embeddings = vocab_size + len(Tokens)
@@ -32,14 +32,14 @@ class TranslationModel(nn.Module):
             dim_output=hparams.frag_dim_embed)
 
         self.encoder = Encoder(
-            hparams=hparams, 
-            rnn_dropout=hparams.rnn_dropout, 
-            num_layers=hparams.rnn_num_layers, 
-            dim_input=hparams.frag_dim_embed, 
+            hparams=hparams,
+            rnn_dropout=hparams.rnn_dropout,
+            num_layers=hparams.rnn_num_layers,
+            dim_input=hparams.frag_dim_embed,
             dim_hidden=hparams.rnn_dim_state)
 
         self.autoencoder = Autoencoder(
-            hparams=hparams, 
+            hparams=hparams,
             output_dir=output_dir)
 
         self.decoder = Decoder(
@@ -56,26 +56,25 @@ class TranslationModel(nn.Module):
         enc_inputs = F.dropout(enc_inputs, p=self.embedding_dropout, training=self.training)
         enc_outputs, enc_hidden = self.encoder(enc_inputs)
         return enc_hidden, enc_outputs
-        
+
     def decode(self, batch, enc_hidden, enc_outputs, dec_inputs):
         dec_inputs = self.embedder(batch, dec_inputs, input=True)
         dec_inputs = F.dropout(dec_inputs, p=self.embedding_dropout, training=self.training)
-        return self.decoder.decode_with_attention(dec_inputs, enc_hidden, enc_outputs)   
-        
+        return self.decoder.decode_with_attention(dec_inputs, enc_hidden, enc_outputs)
+
     def forward(self, batch):
         batch_data, batch_fps, x_enc_inputs, dec_inputs = batch
         x_batch, y_batch = batch_data
         x_fps, y_fps = batch_fps
-        
+
         enc_hidden, x_enc_outputs = self.encode(x_batch, x_enc_inputs)
         y_hat_fps, hidden = self.get_decoder_hidden_state(x_fps)
         dec_hidden = torch.cat([enc_hidden, hidden], dim=-1)
-        
+
         logits = self.decode(y_batch, dec_hidden, x_enc_outputs, dec_inputs)
         return logits, y_hat_fps
-    
+
     def get_decoder_hidden_state(self, x_fps):
-        y_hat_fps, x_enc_hidden = self.autoencoder(x_fps)
+        y_hat_fps, x_enc_hidden = self.autoencoder(x_fps, denoise=False)
         x_enc_hidden = x_enc_hidden.unsqueeze(0).repeat(self.hparams.rnn_num_layers, 1, 1)
         return y_hat_fps, x_enc_hidden
-    
