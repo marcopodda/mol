e183df772522219626a1199b1637046d7a7351a4
Marco Podda <marco.podda@di.unipi.it>
2020-09-11 15:43:15 +0200

    ok

--- core/datasets/preprocess.py
+++ core/datasets/preprocess.py
@@ -1,3 +1,4 @@
+import warnings
 import pandas as pd
 
 from joblib import Parallel, delayed
@@ -66,14 +67,16 @@ def run_preprocess(dataset_name):
     processed_vocab_path = processed_dir / "vocab.csv"
 
     if not processed_data_path.exists():
-        n_jobs = get_n_jobs()
-        clean_fn = getattr(clean_functions, f"clean_{dataset_name}")
-        data = clean_fn(raw_dir, info)
-        cleaned_data = process_data(data[info["smiles_col"]], n_jobs)
-        postprocess_fn = getattr(clean_functions, f"postprocess_{dataset_name}")
-        cleaned_data = postprocess_fn(cleaned_data, data)
-        cleaned_data = cleaned_data.dropna().reset_index(drop=True)
-        cleaned_data.to_csv(processed_data_path)
-        if not processed_vocab_path.exists():
-            vocab = Vocab.from_df(cleaned_data)
-            vocab.save(processed_vocab_path)
+        with warnings.catch_warnings():
+            warnings.filterwarnings('ignore')
+            n_jobs = get_n_jobs()
+            clean_fn = getattr(clean_functions, f"clean_{dataset_name}")
+            data = clean_fn(raw_dir, info)
+            cleaned_data = process_data(data[info["smiles_col"]], n_jobs)
+            postprocess_fn = getattr(clean_functions, f"postprocess_{dataset_name}")
+            cleaned_data = postprocess_fn(cleaned_data, data)
+            cleaned_data = cleaned_data.dropna().reset_index(drop=True)
+            cleaned_data.to_csv(processed_data_path)
+            if not processed_vocab_path.exists():
+                vocab = Vocab.from_df(cleaned_data)
+                vocab.save(processed_vocab_path)
