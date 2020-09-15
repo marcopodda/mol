c65c80bb1bd92f0760a94c8a7f8c79f0c6908a1e
Marco Podda <marco.podda@di.unipi.it>
2020-09-15 23:49:35 +0200

    ok

--- tasks/translation.py
+++ tasks/translation.py
@@ -125,14 +125,14 @@ class TranslationTaskRunner(TaskRunner):
                 state_dict.pop('model.decoder.out.bias')
                 wrapper.load_state_dict(state_dict, strict=False)
 
-            for param in wrapper.model.parameters():
-                param.requires_grad = False
+            # for param in wrapper.model.parameters():
+            #     param.requires_grad = False
 
-            for param in wrapper.model.decoder.attention.parameters():
-                param.requires_grad = True
+            # for param in wrapper.model.decoder.attention.parameters():
+            #     param.requires_grad = True
 
-            wrapper.model.decoder.out.weight.requires_grad = True
-            wrapper.model.decoder.out.bias.requires_grad = True
+            # wrapper.model.decoder.out.weight.requires_grad = True
+            # wrapper.model.decoder.out.bias.requires_grad = True
             return wrapper
 
         return wrapper
