754ce73d44868e436648f09cc13c2b4879142d42
Marco Podda <marco.podda@di.unipi.it>
2020-09-16 23:57:15 +0200

    ok

--- core/datasets/datasets.py
+++ core/datasets/datasets.py
@@ -136,7 +136,7 @@ class EvalDataset(BaseDataset):
 
     def get_input_data(self, index):
         mol_data = self.data.iloc[index]
-        data = self._get_data(mol_data.frags, corrupt=True)
+        data = self._get_data(mol_data.frags, corrupt=False)
         return data
 
 
