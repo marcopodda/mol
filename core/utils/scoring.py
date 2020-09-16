47d4521141370713168ceb86646d481658b45d3a
Marco Podda <marco.podda@di.unipi.it>
2020-09-17 01:03:30 +0200

    ok

--- core/utils/scoring.py
+++ core/utils/scoring.py
@@ -10,12 +10,12 @@ from core.mols.utils import mol_from_smiles
 
 
 SR_KWARGS = {
-    "moses": {"prop_fun": drd2, "similarity_thres": 0.4, "improvement_thres": 0.5},
-    "ZINC": {"prop_fun": drd2, "similarity_thres": 0.4, "improvement_thres": 0.5},
-    "drd2": {"prop_fun": drd2, "similarity_thres": 0.4, "improvement_thres": 0.5},
-    "qed": {"prop_fun": qed, "similarity_thres": 0.4, "improvement_thres": 0.9},
-    "logp04": {"prop_fun": logp, "similarity_thres": 0.4, "improvement_thres": 1.2},
-    "logp06": {"prop_fun": logp, "similarity_thres": 0.4, "improvement_thres": 1.2},
+    "moses": {"prop_fun": drd2, "similarity_thres": 0.3, "improvement_thres": 0.6},
+    "ZINC": {"prop_fun": drd2, "similarity_thres": 0.3, "improvement_thres": 0.6},
+    "drd2": {"prop_fun": drd2, "similarity_thres": 0.3, "improvement_thres": 0.6},
+    "qed": {"prop_fun": qed, "similarity_thres": 0.3, "improvement_thres": 0.6},
+    "logp04": {"prop_fun": logp, "similarity_thres": 0.4, "improvement_thres": 0.8},
+    "logp06": {"prop_fun": logp, "similarity_thres": 0.4, "improvement_thres": 0.8},
 }
 
 
