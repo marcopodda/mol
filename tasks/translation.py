1167499e767a3279472b7dbc4c27f32410d3cbd2
Marco Podda <marco.podda@di.unipi.it>
2020-09-18 06:36:02 +0200

    ok

--- tasks/translation.py
+++ tasks/translation.py
@@ -58,7 +58,7 @@ class TranslationWrapper(Wrapper):
 
 class TranslationSampler(Sampler):
     def prepare_data(self):
-ok        indices = self.dataset.data[self.dataset.data.is_test == True].index.tolist()
+        indices = self.dataset.data[self.dataset.data.is_test == True].index.tolist()
         loader = EvalDataLoader(self.hparams, self.dataset, indices=indices)
         smiles = self.dataset.data.iloc[indices].smiles.tolist()
         return smiles, loader(batch_size=self.hparams.translate_batch_size, shuffle=False)
