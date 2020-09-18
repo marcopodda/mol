cbc758110dfd8640b212dffa9433710dfe8d3ffb
Marco Podda <marco.podda@di.unipi.it>
2020-09-18 13:57:48 +0200

    ok

--- layers/wrapper.py
+++ layers/wrapper.py
@@ -50,7 +50,7 @@ class Wrapper(pl.LightningModule):
 
         decoder_ce_loss = F.cross_entropy(decoder_outputs, decoder_batch.target, ignore_index=0)
         bce_loss = F.binary_cross_entropy_with_logits(mlp_outputs, mlp_targets)
-        cos_sim = F.cosine_similarity(decoder_bag_of_frags, encoder_bag_of_frags)
+        cos_sim = F.cosine_similarity(decoder_bag_of_frags, encoder_bag_of_frags).mean(dim=0)
 
         total_loss = decoder_ce_loss + bce_loss
 
