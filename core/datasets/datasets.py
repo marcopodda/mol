6541ee876a0d98ebd4aa42c8eb658a657156d9fa
Marco Podda <marco.podda@di.unipi.it>
2020-09-18 06:34:50 +0200

    ok

--- core/datasets/datasets.py
+++ core/datasets/datasets.py
@@ -127,7 +127,7 @@ class EvalDataset(BaseDataset):
 
     def get_dataset(self):
         data, vocab, max_length = super().get_dataset()
-        data = data[data.is_val == True].reset_index(drop=True)
+        data = data[data.is_test == True].reset_index(drop=True)
         return data, vocab, max_length
 
     def get_input_data(self, index):
