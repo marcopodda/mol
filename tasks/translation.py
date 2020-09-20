feb0230c490cd2aad6a1c338702587d14e5483e1
Marco Podda <marco.podda@di.unipi.it>
2020-09-20 13:21:25 +0200

    ok

--- tasks/translation.py
+++ tasks/translation.py
@@ -57,16 +57,16 @@ class TranslationWrapper(Wrapper):
         anc_bag_of_frags, pos_bag_of_frags, neg_bag_of_frags = bag_of_frags
 
         decoder_ce_loss = F.cross_entropy(decoder_outputs, anc_batch.target, ignore_index=0)
-        # triplet_loss = F.triplet_margin_loss(anc_bag_of_frags, pos_bag_of_frags, neg_bag_of_frags)
+        triplet_loss = F.triplet_margin_loss(anc_bag_of_frags, pos_bag_of_frags, neg_bag_of_frags)
 
         cos_sim1 = F.cosine_similarity(anc_bag_of_frags, pos_bag_of_frags).mean(dim=0)
         cos_sim2 = F.cosine_similarity(anc_bag_of_frags, neg_bag_of_frags).mean(dim=0)
 
-        total_loss = decoder_ce_loss # + triplet_loss
+        total_loss = decoder_ce_loss + triplet_loss
 
         result = pl.TrainResult(minimize=total_loss)
         result.log('ce', decoder_ce_loss, prog_bar=True)
-        # result.log('tl', triplet_loss, prog_bar=True)
+        result.log('tl', triplet_loss, prog_bar=True)
         result.log('ap', cos_sim1, prog_bar=True)
         result.log('an', cos_sim2, prog_bar=True)
 
