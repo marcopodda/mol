eeafe87b6c6720d5f61d83255e5f8317ae7a2437
Marco Podda <marco.podda@di.unipi.it>
2020-09-21 17:44:13 +0200

    ok

--- core/utils/scoring.py
+++ core/utils/scoring.py
@@ -28,7 +28,7 @@ def novelty(valid_gen, dataset_name):
     data, _, _ = load_data(dataset_name)
     targets = set(data[data.is_y == True].smiles.tolist())
     valid_gen = set(valid_gen)
-    seen = targets & valid
+    seen = targets & valid_gen
     return round(1.0 - len(seen) / len(valid_gen), 4)
 
 
