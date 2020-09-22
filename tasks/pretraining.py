c6bc5643fb6fdb15e9d21f32fb011e962bcbf347
Marco Podda <marco.podda@di.unipi.it>
2020-09-22 11:26:35 +0200

    ok

--- tasks/pretraining.py
+++ tasks/pretraining.py
@@ -12,17 +12,6 @@ class PretrainingWrapper(Wrapper):
     def get_batch_size(self):
         return self.hparams.pretrain_batch_size
 
-    def post_init_wrapper(self, wrapper):
-        print("Loading pretrained model.")
-        state_dict = torch.load(self.pretrain_ckpt)['state_dict']
-        mlp_keys = [k for k in state_dict if "mlp" in k]
-        cl_keys = [k for k in state_dict if "contrastive" in k]
-        print(mlp_keys, cl_keys)
-        [state_dict.pop(k) for k in mlp_keys]
-        [state_dict.pop(k) for k in cl_keys]
-        wrapper.load_state_dict(state_dict, strict=False)
-        return wrapper
-
 
 class PretrainingSampler(Sampler):
     def prepare_data(self):
@@ -36,3 +25,14 @@ class PretrainingSampler(Sampler):
 class PretrainingTaskRunner(TaskRunner):
     wrapper_class = PretrainingWrapper
     sampler_class = PretrainingSampler
+
+    def post_init_wrapper(self, wrapper):
+        print("Loading pretrained model.")
+        state_dict = torch.load(self.pretrain_ckpt)['state_dict']
+        mlp_keys = [k for k in state_dict if "mlp" in k]
+        cl_keys = [k for k in state_dict if "contrastive" in k]
+        print(mlp_keys, cl_keys)
+        [state_dict.pop(k) for k in mlp_keys]
+        [state_dict.pop(k) for k in cl_keys]
+        wrapper.load_state_dict(state_dict, strict=False)
+        return wrapper
