08b46c2bd07432ce9c3c8d99915c3c6f32ca9c11
Marco Podda <marco.podda@di.unipi.it>
2020-09-22 02:37:03 +0200

    ok

--- layers/loss.py
+++ layers/loss.py
@@ -10,14 +10,14 @@ class ContrastiveLoss(nn.Module):
         self.register_buffer("temperature", torch.tensor(temperature))
         self.register_buffer("negatives_mask", (~torch.eye(batch_size, batch_size, dtype=bool)).float())
 
-    def forward(self, emb_i, emb_j):
+    def forward(self, z_i, z_j):
         """
         emb_i and emb_j are batches of embeddings, where corresponding indices are pairs
         z_i, z_j as per SimCLR paper
         """
-        batch_size = emb_i.size(0)
-        z_i = F.normalize(emb_i, dim=1)
-        z_j = F.normalize(emb_j, dim=1)
+        batch_size = z_i.size(0)
+        # z_i = F.normalize(emb_i, dim=1)
+        # z_j = F.normalize(emb_j, dim=1)
 
         similarity_matrix = torch.matmul(z_i, z_j.transpose(1, 0))
 
