34fdbfab912efb4879069f9bc38ba3e0c2a09ae3
Marco Podda <marco.podda@di.unipi.it>
2020-09-22 11:19:10 +0200

    ok

--- tasks/translation.py
+++ tasks/translation.py
@@ -130,8 +130,8 @@ class TranslationTaskRunner(TaskRunner):
             try:
                 wrapper.load_state_dict(state_dict)
             except Exception:
-                mlp_keys = [k for k in state_dict if "mlp" in state_dict]
-                cl_keys = [k for k in state_dict if "contrastive" in state_dict]
+                mlp_keys = [k for k in state_dict if "mlp" in k]
+                cl_keys = [k for k in state_dict if "contrastive" in k]
                 [state_dict.pop(k) for k in mlp_keys]
                 [state_dict.pop(k) for k in cl_keys]
                 state_dict.pop('model.decoder.out.weight')
