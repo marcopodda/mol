0fcd73f68c4eecb82ddd47dbc8a767d766c4094d
Marco Podda <marco.podda@di.unipi.it>
2020-09-22 11:24:08 +0200

    ok

--- tasks/pretraining.py
+++ tasks/pretraining.py
@@ -1,3 +1,4 @@
+import torch
 import numpy as np
 
 from core.datasets.datasets import EvalDataset
@@ -19,6 +20,17 @@ class PretrainingSampler(Sampler):
         loader = EvalDataLoader(self.hparams, self.dataset, indices=indices)
         smiles = self.dataset.data.iloc[indices].smiles.tolist()
         return smiles, loader(batch_size=self.hparams.pretrain_batch_size, shuffle=False)
+
+    def post_init_wrapper(self, wrapper):
+        print("Loading pretrained model.")
+        state_dict = torch.load(self.pretrain_ckpt)['state_dict']
+        mlp_keys = [k for k in state_dict if "mlp" in k]
+        cl_keys = [k for k in state_dict if "contrastive" in k]
+        print(mlp_keys, cl_keys)
+        [state_dict.pop(k) for k in mlp_keys]
+        [state_dict.pop(k) for k in cl_keys]
+        wrapper.load_state_dict(state_dict, strict=False)
+        return wrapper
 
 
 class PretrainingTaskRunner(TaskRunner):
