62bef9e37ce196b306ba5e7b50898512fff7a7aa
Marco Podda <marco.podda@di.unipi.it>
2020-09-22 12:34:08 +0200

    ok

--- layers/autoencoder.py
+++ layers/autoencoder.py
@@ -15,23 +15,29 @@ class Autoencoder(nn.Module):
 
         self.input = nn.Linear(self.dim_input, self.dim_input // 2)
         self.bn_input = nn.BatchNorm1d(self.dim_input // 2)
+
         self.input2hidden = nn.Linear(self.dim_input // 2, self.dim_input // 4)
-        self.bn_input2hidden = nn.BatchNorm1d(self.dim_input // 4)
+        self.bn_input2hidden = nn.BatchNorm1d(self.input2hidden.out_features)
+
         self.hidden2bottleneck = nn.Linear(self.dim_input // 4, self.dim_hidden)
-        self.bn_hidden2bottleneck = nn.BatchNorm1d(self.dim_hidden)
+        self.bn_hidden2bottleneck = nn.BatchNorm1d(self.hidden2bottleneck.out_features)
+
         self.bottleneck2hidden = nn.Linear(self.dim_hidden, self.dim_input // 4)
-        self.bn_bottleneck2hidden = nn.BatchNorm1d(self.dim_input // 4)
+        self.bn_bottleneck2hidden = nn.BatchNorm1d(self.bottleneck2hidden.out_features)
+
         self.hidden2output = nn.Linear(self.dim_input // 4, self.dim_input // 2)
+        self.bn_hidden2output = nn.BatchNorm1d(self.hidden2output.out_features)
+
         self.output = nn.Linear(self.dim_input // 2, self.dim_input)
 
     def encode(self, inputs):
-        x = self.input(inputs)
-        x = self.input2hidden(F.relu(self.bn_input(x)))
-        return self.hidden2bottleneck(F.relu(self.bn_input2hidden(x)))
+        x = F.relu(self.bn_input(self.input(inputs)))
+        x = F.relu(self.bn_input2hidden(self.input2hidden(x)))
+        return self.hidden2bottleneck(x)
 
     def decode(self, hidden):
-        x = self.bottleneck2hidden(F.relu(self.bn_hidden2bottleneck(hidden)))
-        x = self.hidden2output(F.relu(self.bn_bottleneck2hidden(x)))
+        x = F.relu(self.bn_bottleneck2hidden(self.bottleneck2hidden(hidden)))
+        x = F.relu(self.bn_hidden2output(self.hidden2output(x)))
         return self.output(x)
 
     def forward(self, batch):
