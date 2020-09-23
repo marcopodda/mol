a84fa66d8ab6ef25c9a692bbd45d59ec8d57f2fa
Marco Podda <marco.podda@di.unipi.it>
2020-09-23 10:42:41 +0200

    ok

--- layers/wrapper.py
+++ layers/wrapper.py
@@ -65,8 +65,8 @@ class Wrapper(pl.LightningModule):
         n = torch.matmul(anc_bag, neg_bag.transpose(1, 0)).sum()
         closs = -(F.logsigmoid(p) + F.logsigmoid(-n))
 
-        p = torch.matmul(pos_fp_outputs, anc_batch.transpose(1, 0)).sum()
-        n = torch.matmul(neg_fp_outputs, anc_batch.transpose(1, 0)).sum()
+        p = torch.matmul(pos_fp_outputs, anc_fp_target.transpose(1, 0)).sum()
+        n = torch.matmul(neg_fp_outputs, anc_fp_target.transpose(1, 0)).sum()
         fploss = -(F.logsigmoid(p) + F.logsigmoid(-n))
 
         total_loss = pos_loss + closs + fploss
